566541240b63aacd1adb1f73309a990713abb5b6
vaziliybober <vaziliybober@gmail.com>
2020-11-02 01:33:46 +0300

    add form tests

--- __tests__/form.test.js
+++ __tests__/form.test.js
@@ -1,19 +1,20 @@
 /* eslint-disable no-underscore-dangle */
 
 import nock from 'nock';
-import axios from 'axios';
-import httpAdapter from 'axios/lib/adapters/http.js';
 import prettier from 'prettier';
 import path from 'path';
-import fs from 'fs';
-// import { promises as fsp } from 'fs';
-import timer from 'timer-promise';
+import fs, { promises as fsp } from 'fs';
+import { waitFor } from '@testing-library/dom';
+import '@testing-library/jest-dom';
+import axios from 'axios';
+import httpAdapter from 'axios/lib/adapters/http';
+import i18next from 'i18next';
 import run from '../src/application.js';
 
 axios.defaults.adapter = httpAdapter;
 nock.disableNetConnect();
 
-// const getFixturePath = (filepath) => path.join(__dirname, '..', '__fixtures__', filepath);
+const getFixturePath = (filepath) => path.join(__dirname, '..', '__fixtures__', filepath);
 
 const options = {
   parser: 'html',
@@ -21,21 +22,20 @@ const options = {
   tabWidth: 4,
 };
 
+let elements;
+
 const prettify = (html) => prettier.format(html, options);
+const getSectionTree = () => prettify(elements.section.innerHTML);
 
 beforeEach(() => {
   const initHtml = fs.readFileSync(path.join(__dirname, '..', 'index.html')).toString();
   document.documentElement.innerHTML = initHtml;
-  run();
-});
 
-test('form', async () => {
   const section = document.querySelector('section.jumbotron');
-  const getSectionTree = () => prettify(section.innerHTML);
-
   const form = section.querySelector('form');
 
-  const elements = {
+  elements = {
+    section,
     form,
     input: form.querySelector('input'),
     submit: form.querySelector('button'),
@@ -43,19 +43,62 @@ test('form', async () => {
     postsContainer: document.querySelector('.posts'),
   };
 
-  expect(getSectionTree()).toMatchSnapshot();
+  run();
+});
 
+test('form initial', async () => {
+  expect(getSectionTree()).toMatchSnapshot();
+});
+
+test('form invalid url', async () => {
   elements.input.value = 'asdf';
   elements.submit.click();
-  await timer.start(10);
+  await waitFor(() => {
+    expect(elements.section).toHaveTextContent(i18next.t('form.errors.invalidUrl'));
+  });
+  expect(getSectionTree()).toMatchSnapshot();
+});
+
+test('form invalid RSS', async () => {
+  elements.input.value = 'https://ru.hexlet.io/lessons.rss';
+  nock('https://cors-anywhere.herokuapp.com')
+    .get('/https://ru.hexlet.io/lessons.rss')
+    .reply(200, 'some invalid <pipipu> rss');
+  elements.submit.click();
+  await waitFor(() => {
+    expect(elements.section).toHaveTextContent(i18next.t('form.errors.invalidRss'));
+  });
+  expect(getSectionTree()).toMatchSnapshot();
+});
+
+test('form network error', async () => {
+  elements.input.value = 'https://ru.hexletttttttt.io/lessons.rss';
+  nock('https://cors-anywhere.herokuapp.com')
+    .get('/https://ru.hexletttttttt.io/lessons.rss')
+    .replyWithError('some error');
+  elements.submit.click();
+  await waitFor(() => {
+    expect(elements.section).toHaveTextContent(i18next.t('form.errors.networkError'));
+  });
+  expect(getSectionTree()).toMatchSnapshot();
+});
+
+test("form rss loaded and can't load same", async () => {
+  elements.input.value = 'https://ru.hexlet.io/lessons.rss';
+  const rss = await fsp.readFile(getFixturePath('rss.xml'));
+  nock('https://cors-anywhere.herokuapp.com')
+    .get('/https://ru.hexlet.io/lessons.rss')
+    .reply(200, rss);
+  elements.submit.click();
+  await waitFor(() => {
+    expect(elements.section).toHaveTextContent(i18next.t('form.hints.rssLoaded'));
+  });
   expect(getSectionTree()).toMatchSnapshot();
 
-  // elements.input.value = 'https://ru.hexlet.io/lessons.rss';
-  // const rss = await fsp.readFile(getFixturePath('rss.xml'));
-  // nock('https://cors-anywhere.herokuapp.com')
-  //   .get('/https://ru.hexlet.io/lessons.rss')
-  //   .reply(200, rss);
-  // elements.submit.click();
-  // await timer.start(10);
-  // expect(getSectionTree()).toMatchSnapshot();
+  elements.input.value = 'https://ru.hexlet.io/lessons.rss';
+  elements.submit.click();
+  await waitFor(() => {
+    expect(elements.section).toHaveTextContent(i18next.t('form.errors.repetativeUrl'));
+  });
+  expect(getSectionTree()).toMatchSnapshot();
 });
