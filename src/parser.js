2948c2096b479b4a98ef5bc676ba6005f76b821b
vaziliybober <vaziliybober@gmail.com>
2020-11-03 00:53:43 +0300

    add return of parse errors

--- src/parser.js
+++ src/parser.js
@@ -1,17 +1,23 @@
 const parse = (rss) => {
   const parser = new DOMParser();
   const doc = parser.parseFromString(rss, 'text/xml');
+  const errors = Array.from(doc.getElementsByTagName('parsererror'))
+    .map(err => err.innerHTML);
 
-  const title = doc.querySelector('title').textContent;
-  const postElements = Array.from(doc.querySelectorAll('item'));
+  try {
+    const title = doc.querySelector('title').textContent;
+    const postElements = Array.from(doc.querySelectorAll('item'));
+    const posts = postElements.map((postElem) => ({
+      title: postElem.querySelector('title').textContent,
+      link: postElem.querySelector('link').textContent,
+      guid: postElem.querySelector('guid').textContent,
+    }));
 
-  const posts = postElements.map((postElem) => ({
-    title: postElem.querySelector('title').textContent,
-    link: postElem.querySelector('link').textContent,
-    guid: postElem.querySelector('guid').textContent,
-  }));
-
-  return { title, posts };
+    return { title, posts, errors };
+  }
+  catch (err) {
+    return { errors: [...errors, err] };
+  }
 };
 
 export default parse;
